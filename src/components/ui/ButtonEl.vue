113a962696398f1f6b252463d7c4ed8ff956e82f
FAST <krinickij.v.a@gmail.com>
2024-11-08 10:00:06 +0300

    FEAT: aded buttonEl component
    Добавлен компонент кнопки
    Кнопка в компоненте карточки заменена на компонент кнопки

---
+++ src/components/ui/ButtonEl.vue
@@ -0,0 +1,10 @@
+<template>
+    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 font-bold">
+        addToCard
+    </button>
+</template>
+<script setup>
+
+</script>
+<style>
+</style>
