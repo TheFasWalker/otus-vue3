1f14712e14b3ded3fa86af7940532c2902ec2751
FAST <krinickij.v.a@gmail.com>
2024-11-09 11:26:40 +0300

    FEAT: aded situation when we have no itens to show
    Добавлено отображение пустого списка элементов.

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div class="antialiased bg-gray-50 dark:bg-gray-900">
     <HeaderComp />
     <Sidebar />
-    <main class="p-4 md:ml-64 h-auto pt-20">
+    <main class="p-4 md:ml-64 pt-20 h-[100vh] overflow-x-auto">
 
       <section class="bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-12">
         <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
-
-          <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
+        <div v-if="!productsRef.length" class="flex items-center justify-center">
+          <span class="text-center">Чт ото пошло не так... <br> товаров нет...</span>
+          
+        </div>
+          <div v-if="productsRef.length" class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
             <ProductCard v-for="product in productsRef" :key="product.id" />
           </div>
-          <div class="w-full text-center">
+          <div v-if="productsRef.length" class="w-full text-center">
             <button type="button"
               class="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">Show
               more</button>
@@ -28,8 +31,10 @@ import Sidebar from './components/general/Sidebar.vue';
 import ProductCard from './components/ProductCard.vue';
 import Loader from './components/general/Loader.vue';
 
+
+
 const productsRef = ref([
-  {
+{
     "id": 1,
     "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
     "price": 109.95,
