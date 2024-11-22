ea7d541a4493466e496f65ffb57aa3df3e052e2b
FAST <krinickij.v.a@gmail.com>
2024-11-22 10:59:33 +0300

    FEAT: aded dropdown selector
    Добавлен компонент  дропдауна - выбора

--- src/components/general/Forms/AddItemForm.vue
+++ src/components/general/Forms/AddItemForm.vue
@@ -9,20 +9,36 @@
     >
     <form class="flex flex-col gap-5 items-center w-full">
         <h1 class="  text-gray-900 text-xl ">Добавить товар</h1>
-        <InputComp
-            title="title"
+            <InputComp
+            title="Название"
             type="text"
             palaceholder="placeholder"
-            v-model="inputText"
-            name="title"
+            v-model.trim="formData.itemName"
+            name="itemName"
             />    
+            <InputComp
+            title="Количество"
+            type="number"
+            palaceholder="itemsCount"
+            v-model.trim="formData.itemsCount"
+            name="itemName"
+            /> 
+
+            <DropDownSelect
+            title="Страна производитель"
+            name="sity"
+            :dataElems="dropDownData"
+            v-model="formData.sity"
+            />
+
             <TextAreaComp
             title="Описание"
             name="description"
             placeholder="описание"
-            v-model="textArea"/>
+            v-model.trim="formData.textArea"/>
         <ButtonEl
             title="Добавить товар"
+            @click='console.log(formData)'
         />
     </form>
 
@@ -30,15 +46,30 @@
 </template>
 <script setup>
 
-import { ref } from 'vue';
+import { ref,reactive } from 'vue';
 import ButtonWhite from '../../ui/buttons/ButtonWhite.vue';
 import PopupLauout from '../Lauouts/PopupLauout.vue';
 import InputComp from '../../ui/formItems/InputComp.vue';
 import ButtonEl from '../../ui/ButtonEl.vue';
 import TextAreaComp from '../../ui/formItems/TextAreaComp.vue';
+import DropDownSelect from '../../ui/formItems/DropDownSelect.vue';
 const popupState = ref(true)
 
-const inputText = ref('inputText')
-const textArea= ref('textArea')
-
+const formData = reactive({
+    itemName:'',
+    textArea:'',
+    itemsCount:0,
+    sity:''
+})
+const dropDownData = ref([
+    {title:'Россия',
+        name:'rus'
+    },
+    {title:'Казахстан',
+        name:'kz'
+    },
+    {title:'Англия',
+        name:'uk'
+    },
+])
 </script>
---
+++ src/components/ui/formItems/DropDownSelect.vue
@@ -0,0 +1,26 @@
+<template>
+<label class="w-full flex flex-col gap-1">
+    <span>{{ title }}</span>
+    <select :name=name @input="$emit('update:modelValue',$event.target.value)">
+        <option 
+        value=""
+        :selected = "modelValue === '' || modelValue === ' ' || modelValue==='notSelected'"
+
+        >Не указано</option>
+
+        <option 
+            v-for="data in dataElems" 
+            :value=data.name 
+            :selected = "modelValue === data.name"
+            >
+            {{ data.title }}
+        </option>
+    </select>
+</label>
+{{ modelValue }}
+</template>
+<script setup>
+
+defineProps(['title', 'name', 'dataElems','modelValue'])
+
+</script>
