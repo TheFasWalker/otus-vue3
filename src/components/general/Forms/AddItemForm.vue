6bc298aad6840e9803b5705257024035b4384fda
FAST <krinickij.v.a@gmail.com>
2024-11-23 16:56:16 +0300

    FEAT : aded sending function for creating item popup form
    Добавлена функция отправки данных для формы в попапе для создания элемента товара

--- src/components/general/Forms/AddItemForm.vue
+++ src/components/general/Forms/AddItemForm.vue
@@ -6,61 +6,72 @@
     <PopupLauout 
     v-if="popupState"
     @close-popup="popupState = !popupState"
-    ><div class="">
-    <form class="flex flex-col gap-5 items-center w-full" @submit.prevent="formSubmit">
-        <div class=""></div>
-        <h1 class="  text-gray-900 text-xl ">Добавить товар</h1>
-            <InputComp
-                title="Название"
-                type="text"
-                palaceholder="placeholder"
-                v-model.trim="itemName"
-                v-bind="itemNameAttrs"
-                name="itemName"
-                :error="errors.itemName"
-
-            />    
-
-            <InputComp
-                title="Количество"
-                type="number"
-                palaceholder="itemsCount"
-                v-bind="itemsCountAttrs"
-                v-model.trim="itemsCount"
-                name="itemCount"
-                :error="errors.itemsCount"
-
-            /> 
-
-            <DropDownSelect
-                title="Страна производитель"
-                name="sity"
-                :dataElems="dropDownData"
-                v-model="sity"
-                v-bind="sityAttrs"
-                :error="errors.sity"
-            />
-
-            <TextAreaComp
-                title="Описание"
-                name="description"
-                placeholder="описание"
-                v-model.trim="description"
-                v-bind="descriptionAttrs"
-            />
-            <Checkbox
-                title="Чекбокс"
-                name="activity"
-                v-model="activity"
-            /> 
-
-        <ButtonEl
-            type="submit"
-            title="Добавить товар"
+    >
+    <Loader v-if="loading"/>
+    <div class="" v-if="!errorData">
+        <div v-if="formSendedSucsess" class="">
             
-        />
-    </form>
-</div>
+            <h1>Товар успешно сохранен в базу </h1>
+        </div>
+        <form v-else class="flex flex-col gap-5 items-center w-full" @submit.prevent="formSubmit">
+
+            <h1 class="  text-gray-900 text-xl ">Добавить товар</h1>
+                <InputComp
+                    title="Название"
+                    type="text"
+                    palaceholder="placeholder"
+                    v-model.trim="itemName"
+                    v-bind="itemNameAttrs"
+                    name="itemName"
+                    :error="errors.itemName"
+
+                />    
+
+                <InputComp
+                    title="Количество"
+                    type="number"
+                    palaceholder="itemsCount"
+                    v-bind="itemsCountAttrs"
+                    v-model.trim="itemsCount"
+                    name="itemCount"
+                    :error="errors.itemsCount"
+
+                /> 
+
+                <DropDownSelect
+                    title="Страна производитель"
+                    name="sity"
+                    :dataElems="dropDownData"
+                    v-model="sity"
+                    v-bind="sityAttrs"
+                    :error="errors.sity"
+                />
+
+                <TextAreaComp
+                    title="Описание"
+                    name="description"
+                    placeholder="описание"
+                    v-model.trim="description"
+                    v-bind="descriptionAttrs"
+                />
+                <Checkbox
+                    title="Чекбокс"
+                    name="activity"
+                    v-model="activity"
+                    v-bind="activityAttrs"
+                /> 
+
+            <ButtonEl
+                type="submit"
+                title="Добавить товар"
+                
+            />
+        </form>
+    </div>
+    <div class=""v-else>
+        <h1>Произошла непредвиденная ошибка</h1>
+        {{ errorData }}
+    </div>
 
     </PopupLauout>
 </template>
@@ -77,28 +88,61 @@ import Checkbox from '../../ui/formItems/Checkbox.vue';
 import { useForm } from 'vee-validate';
 import {toTypedSchema} from '@vee-validate/yup'
 import * as yup from 'yup'
+import Loader from '../Loader.vue'
+
+
+
+
+
+const formSendedSucsess = ref(false)
+const popupState = ref(false)
+const loading = ref(false)
+const errorData = ref('')
 
 const schema = toTypedSchema(yup.object({
     itemName:yup.string().required('Обязательное поле').min(3,'Минимум 3 символа'),
     itemsCount:yup.number().required('укажите количемство'),
     sity:yup.string().required('Укажите страну'),
-    description:yup.string(),
-    activity: yup.boolean()
+    description:yup.string().nullable(),
+    activity: yup.mixed().oneOf([true, false, null, undefined]),
 }))
 const {errors,defineField, handleSubmit}=useForm({
-    validationSchema:schema
+    validationSchema:schema,
+    validateOnInput:true, 
+    validateOnChange:true,
+    strategy: 'individual'
 })
 const [itemName, itemNameAttrs]= defineField('itemName')
 const [itemsCount, itemsCountAttrs]= defineField('itemsCount')
 const [sity, sityAttrs]= defineField('sity')
-const [description, descriptionAttrs]= defineField('description')
-const [activity, activityAttrs]= defineField('activity')
+const [description, descriptionAttrs]= defineField('description',{value:formData.description})
+const [activity, activityAttrs]= defineField('activity',{value:false})
 
-const popupState = ref(true)
 
-const formSubmit = handleSubmit(values=>{
-    console.log(values)
-    console.log('sending')
+
+const formSubmit = handleSubmit(async(values)=>{
+    try{
+        loading.value=true
+        const response = await fetch('https://httpbin.org/anything ', { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(values)
+            });
+            console.log('sss')
+        if(response.ok){
+            formSendedSucsess.value=true
+        }else{
+            console.log('error',response.error)
+            errorData.value = response.error
+            throw new Error('Ошибка сервера')
+        }
+    }catch(error){
+        errorData.value = error
+    }finally{
+        loading.value=false
+    }
 })
 const dropDownData = ref([
     {title:'Россия',
