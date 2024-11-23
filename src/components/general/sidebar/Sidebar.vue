d03225dc5d8d331ea8e7a8d0b0cead73b491add9
FAST <krinickij.v.a@gmail.com>
2024-11-23 19:56:49 +0300

    FEAT: adede conponent for toggling localhost "token"
    Добавлен компонент для тогла токена в localstorage
    Удалелы ненужыне ссылки в сайдбаре

--- src/components/general/sidebar/Sidebar.vue
+++ src/components/general/sidebar/Sidebar.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ToggleTokenConponent from '../../TemporaryFiles/ToggleTokenConponent.vue';
 import DropDown from '../../ui/sidebar/DropDown.vue';
 import SidebarMainLink from './sidebarMainLink.vue';
 
@@ -33,65 +34,15 @@ import SidebarMainLink from './sidebarMainLink.vue';
           </li>
 
           <li>
-<DropDown/>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-            >
-              <svg
-                aria-hidden="true"
-                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"
-                ></path>
-                <path
-                  d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"
-                ></path>
-              </svg>
-              <span class="flex-1 ml-3 whitespace-nowrap">Messages</span>
-              <span
-                class="inline-flex justify-center items-center w-5 h-5 text-xs font-semibold rounded-full text-primary-800 bg-primary-100 dark:bg-primary-200 dark:text-primary-800"
-              >
-                4
-              </span>
-            </a>
+          <DropDown/>
           </li>
 
-        </ul>
-        <ul
-          class="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700"
-        >
-          <li>
-            <a
-              href="#"
-              class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
-            >
-              <svg
-                aria-hidden="true"
-                class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-                <path
-                  fill-rule="evenodd"
-                  d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span class="ml-3">Docs</span>
-            </a>
-          </li>
 
         </ul>
+
+      <ToggleTokenConponent/>
       </div>
+
     </aside>
 </template>
 <style >
