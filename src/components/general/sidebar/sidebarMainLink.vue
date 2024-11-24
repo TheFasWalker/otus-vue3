70b452fb5aa8d49c233ded5f569cee396dd95c62
FAST <krinickij.v.a@gmail.com>
2024-11-24 11:41:42 +0300

    FEAT: refactor + add activity styles for sidebarlink

--- src/components/general/sidebar/sidebarMainLink.vue
+++ src/components/general/sidebar/sidebarMainLink.vue
@@ -1,12 +1,11 @@
 <template>
-<router-link :to="{name:  nav }">
-    <a
-    href="#"
-    class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-    >
+<router-link 
+:to="{name:  nav }"
+class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg  hover:bg-gray-100  group"
+>
     <svg
         aria-hidden="true"
-        class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+        class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 "
         fill="currentColor"
         viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
@@ -15,9 +14,13 @@
         <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
     </svg>
     <span class="ml-3">{{ title }}</span>
-    </a>
     </router-link>
 </template>
 <script setup>
 defineProps(['title','nav'])
 </script>
+<style lang="css" scoped>
+    .router-link-active{
+        background-color: #e1d9d9; 
+    }
+</style>
