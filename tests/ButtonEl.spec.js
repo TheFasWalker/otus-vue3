651e1e84f7701593335c50cced8bca1899a2d6c7
FAST <krinickij.v.a@gmail.com>
2024-12-07 11:48:23 +0300

    FEAT: aded base test settings

---
+++ tests/ButtonEl.spec.js
@@ -0,0 +1,10 @@
+import { describe ,it } from "vitest";
+import { mount } from "@vue/test-utils";
+import component from '../src/components/ui/ButtonEl.vue'
+
+describe('buttonElement testing',()=>{
+
+    it('mount without errors',()=>{
+
+    })
+})
